6c09f3ea6f1b3a956b68a87d6d2f12f1fd5fa907
youngsik_won <youngsik3200@gmail.com>
2020-09-03 20:34:35 +0900

    Auto Layout

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -9,12 +9,28 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var slider: UISlider! // 스토리 보드에 연결된 요소 어노테이션.
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    //Slider
+   @IBAction func sliderValueChanged(_ sender: UISlider){
+    print(sender.value)
+    }
+    // UIButton Action
+   @IBAction func touchUpHitButton(_ sender: UIButton) {
+    print(slider.value)
+    }
+    
+    
+    @IBAction func touchUpResetButton(_ sender: UIButton){
+        print("touch Up reset Button")
+    }
 
 }
 
