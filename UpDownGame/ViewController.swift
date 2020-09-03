259ef4acf7b9ad9aaa0652b4b2ab4ad4372ce425
youngsik_won <youngsik3200@gmail.com>
2020-09-03 21:40:16 +0900

    random 함수  초기화

--- UpDownGame/ViewController.swift
+++ UpDownGame/ViewController.swift
@@ -10,14 +10,22 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    @IBOutlet weak var slider: UISlider! // 스토리 보드에 연결된 요소 어노테이션.
     
+    var randomValue: Int = 0
+    var tryCount: Int = 0
+    
+    
+    @IBOutlet weak var slider: UISlider! // 스토리 보드에 연결된 요소 어노테이션.
+    @IBOutlet weak var tryCountLabel: UILabel!
+    @IBOutlet weak var sliderValueLabel: UILabel!
+    @IBOutlet weak var minimumValueLabel: UILabel!
+    @IBOutlet weak var maximumValueLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    
         //slider.setThumbImage(#imageLiteral(resourceName: T##String), for: .normal) //이미지 넣기
+        reset()
     }
     
     //Slider
@@ -27,11 +35,18 @@ class ViewController: UIViewController {
     // UIButton Action
    @IBAction func touchUpHitButton(_ sender: UIButton) {
     print(slider.value)
+    
     }
     
     
     @IBAction func touchUpResetButton(_ sender: UIButton){
         print("touch Up reset Button")
+        reset()
+    }
+    
+    @IBAction func reset(){
+        print("reseåt!")
+        randomValue = Int.random(in: 0...30) // ...은 범위 연산자의 일종
     }
 
 }
